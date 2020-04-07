36af4ffd3dbb7594b6b0f5b021cfed5328874ff8
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-07 22:34:43 +0530

    structed for new site

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/app.scss';
+import Home from "./Home/Home";
+
+
+function App() {
+    return (
+        <div className="App">
+           <Home/>
+        </div>
+    );
+}
+
+export default App;

91adefd10683a3161628355dfb317a6d9fc55876
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-09 04:55:24 +0530

    menu added

--- src/components/BottomPeak.js
+++ src/components/BottomPeak.js
@@ -4,14 +4,28 @@ const transition = ()=> {
 
 };
 
+const Menu = [
+    {displayName:"Home", subtitle:"Know everything about us.", link:"/"},
+    {displayName:"About", subtitle:"Why a conceptualization studio?", link:"/about"},
+    {displayName:"Services", subtitle:"What all can we do together?", link:"/services"},
+    {displayName:"Clients", subtitle:"You are in a good company.", link:"/clients"},
+    {displayName:"Just a message away!", subtitle:"bescarlet.in@gmail.com", link:"mailto:bescarlet.in@gmail.com"}
+];
+
+
 function BottomPeak(props){
     return(
         <div className={'bottom-peak-container-primary'}>
             <div className={'menu'}>
-                <h1> Home </h1>
-                <h1> About </h1>
-                <h1> Services </h1>
-                <h1> Clients </h1>
+                {
+                    Menu.map(menu => (
+                        <a href={menu.link}>
+                            <h1>  {menu.displayName}
+                                <h3> {menu.subtitle} </h3>
+                            </h1>
+                        </a>
+                    ))
+                }
             </div>
         </div>
     );

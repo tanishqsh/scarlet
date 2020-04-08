bf22f7965b60979359f9a598ad4989dacd957f7f
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-09 04:01:58 +0530

    added a temporary menu

--- src/components/BottomPeak.js
+++ src/components/BottomPeak.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const transition = ()=> {
+
+};
+
 function BottomPeak(props){
     return(
         <div className={'bottom-peak-container-primary'}>
-
+            <div className={'menu'}>
+                <h1> Home </h1>
+                <h1> About </h1>
+                <h1> Services </h1>
+                <h1> Clients </h1>
+            </div>
         </div>
     );
 }

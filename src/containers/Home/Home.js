20301d3bced28fb7e91f157785b08154869d3f50
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-08 23:14:14 +0530

    started the homepage

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,8 +3,14 @@ import React from 'react';
 
 function Home() {
     return(
-        <div className={'home-container'}>
-            <h1> Scarlet. </h1>
+        <div className={'home-container container-fluid'}>
+            <div className={'row'}>
+                <div className={'col-sm-2'}/>
+                <div className={'col-sm-8'}>
+                    <h1> Conceptualization & Implementation Studio.  </h1>
+                </div>
+                <div className={'col-sm-2'}/>
+            </div>
         </div>
     );
 }

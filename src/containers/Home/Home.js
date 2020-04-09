35883fa0531e131099a6a0560110425568f9aa3e
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-09 23:10:10 +0530

    added concept studio page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import BottomPeak from "../../components/BottomPeak";
 import Frame from "../Frame/Frame";
-import Cursor from "../../components/Cursor";
+import StudioConcept from "../StudioConcept/StudioConcept";
 
 
 function Home() {
     return(
         <div className={'home-container container-fluid'}>
+            <StudioConcept/>
             <BottomPeak class={'primary'}/>
             <Frame/>
             <div className={'row inner-container'}>
                 <div className={'col-sm-2'}/>
                 <div className={'col-sm-7'}>
-                    <h1> Conceptualisation & Implementation Studio </h1>
+                    <h1 className={'title'}> Conceptualisation & Implementation <span className={'alt'}> Studio </span> </h1>
                     <h3> A conceptualisation studio that expertises in advertising, branding
                         and product development. </h3>
                 </div>

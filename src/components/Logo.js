863196fac34509f703118ddbd6e927feaa206752
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-12 05:28:53 +0530

    minor changes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ function Logo(props){
     return(
         <div className={'logo-container-'+props.theme}>
             <Link to={'/'}>
-                <h2> Scarlet. </h2>
+                <h2> Scarlet.</h2>
             </Link>
         </div>
     );

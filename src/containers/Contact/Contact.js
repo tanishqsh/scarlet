af35211c35df05c6bb1aaa1666c6cbb0364ae2a1
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-11 21:11:16 +0530

    minor changes

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Frame from "../Frame/Frame";
 import Back from "../../components/Back";
 import CoffeeCup from '../../images/coffee.png';
 import CurrentDate from "../../components/CurrentDate";
--- src/containers/StudioConcept/StudioConcept.js
+++ src/containers/StudioConcept/StudioConcept.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function StudioConcept(props) {
 
-    const content = "You have an idea but you are unsure of how to bring it to life. So it remains an idea for a long long time, but now  it is time for it to happen – so you come to us, our team understands your outcomes, inputs and your desired result. The brains behind scarlet sit down together to find everything that is required to bring your idea to life, and then we turn it into a concept. Once the concept is created, our team takes it through implementation.";
+    const content = "You have an idea but you are unsure of how to bring it to life. So it remains an idea for a long long time, but now  it is time for it to happen – so you come to us, our team understands your outcomes, inputs and your desired result. The brains behind Scarlet. sit down together to find everything that is required to bring your idea to life, and then we turn it into a concept. Once the concept is created, our team takes it through implementation.";
 
     return(
         <div className={'studio-concept-container'+props.currentState}>

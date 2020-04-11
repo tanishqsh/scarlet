8091feaf12b1179bc16c2a699d15697cf52d0310
Tanishq Sharma <tanishqsh@gmail.com>
2020-04-11 07:51:12 +0530

    call to action component with action text and link

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom";
 function CallToAction(props) {
     return(
         <div className={'call-to-action-container-'+props.theme}>
-            <Link to={'/contact'}>
-                <h2> A touch away. </h2>
+            <Link to={props.actionLink?props.actionLink:'/contact'}>
+               {props.actionText?<h2> {props.actionText} </h2>: <h2> A touch away. </h2> }
             </Link>
         </div>
     )
--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Frame from "../Frame/Frame";
 import Back from "../../components/Back";
 import CoffeeCup from '../../images/coffee.png';
+import CurrentDate from "../../components/CurrentDate";
+import SocialLinks from "../../components/SocialLinks";
+import Logo from "../../components/Logo";
+import CallToAction from "../../components/CallToAction";
 
 function Contact() {
     return(
         <div className={'page-container-primary'}>
-            <Frame theme={'secondary'}/>
+            <CurrentDate theme={'secondary'}/>
+            <SocialLinks theme={'secondary'}/>
+            <Logo theme={'secondary'}/>
+            <CallToAction actionText={'Go Home'} actionLink={'/'} theme={'secondary'}/>
             <Back changeState={()=>{}} theme={'secondary'}/>
             <div className={'contact-page-container'}>
                 <h1>  </h1>

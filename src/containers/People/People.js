e1abe23f35962d8f7a5760e4afdf6345a8113e51
Tanishq Sharma <tanishqsh@gmail.com>
2020-05-17 18:28:12 +0530

    people page

--- src/containers/People/People.js
+++ src/containers/People/People.js
@@ -21,11 +21,18 @@ export default function People(){
             <div className={'row'}>
                 <div className={'col-sm-3'}/>
                 <div className={'col-sm-3 text-left'}>
-                    {}
+                    <h1> Tanishq </h1>
+                    <h3> Full Stack Developer, Brand Designer and a Psychological Driven Marketer at the core. Founded Scarlet, TheLabs and Whoosh.
+                        With over 8 years of product & design development, psychology-driven advertising, I have helped countless brands in India, UK and UAE refine and implement their ideas. With a platform that curates educational short term programs outside India, I have helped countless students to get an experience of international education.
+                        With a brand that focuses on revolutionary new designs, I have helped over tens of thousands of customers at Whoosh, to re-define their fashion.
+                        I am a conceptualist and this is my story.
+                    </h3>
                 </div>
                 <div className={'col-sm-3 text-left'}>
                     <h1> Divyang </h1>
-                    <h3> An entrepreneur with a demonstrated history of Startup creation and business development. Skilled in system modeling, application development, marketing, web development, user experience and a strong business development professional with a B. Tech + MBA focused on Marketing, Operations and Information Technology from SVKM's Narsee Monjee Institute of Management Studies (NMIMS).
+                    <h3> I am a marketer with an interest in consumer neuroscience and psychology-based marketing, building the best of brands. I have spent the last couple of years mastering consultancy and strategy
+                        in the product as well as the service sector. As a founder, I have been fueling Scarlet. and TheLabs with a great amount of dynamism. Working on new and impactful ideas is the best source of amusement for me.
+                        Always up for virtual coffee and good brainstorming sessions. I call myself an explorer, ahoy!
                     </h3>
                     <hr/>
                     <h4> <a target={'blank'} href={'https://www.linkedin.com/in/divyang-kaushik-672b37142/'}> Connect </a> </h4>

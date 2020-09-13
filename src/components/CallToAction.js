f4e94d676038b6c9fdc570866399a785ad10b660
Tanishq Sharma <tanishqsh@gmail.com>
2020-09-13 21:01:38 +0530

    Update

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function CallToAction(props) {
-    return(
-        <div className={'call-to-action-container-'+props.theme}>
-            <Link to={props.actionLink?props.actionLink:'/contact'}>
-               {props.actionText?<h2> {props.actionText} </h2>: <h2> A touch away. </h2> }
-            </Link>
-        </div>
-    )
+  return (
+    <div className={'call-to-action-container-' + props.theme}>
+      <Link to={props.actionLink ? props.actionLink : '/contact'}>
+        {props.actionText ? (
+          <h2> {props.actionText} </h2>
+        ) : (
+          <h2> Talk to our sales team </h2>
+        )}
+      </Link>
+    </div>
+  );
 }
 
 export default CallToAction;

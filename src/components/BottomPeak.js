0ebc225d21d0cf4604d0fd89c98bf4c7f50663f1
Tanishq Sharma <tanishqsh@gmail.com>
2020-09-13 20:57:21 +0530

    Update

--- src/components/BottomPeak.js
+++ src/components/BottomPeak.js
@@ -1,47 +1,69 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Menu = [
-    {displayName:"Home", subtitle:"Know everything about us.", link:"/"},
-    {displayName:"About", subtitle:"Why a conceptualization studio?", link:"/about"},
-    {displayName:"Services", subtitle:"What all can we do together?", link:"/services"},
-    {displayName:"Clients", subtitle:"You are in a good company.", link:"/clients"},
-    {displayName:"Mail Us", subtitle:"bescarlet.in@gmail.com", link:"mailto:bescarlet.in@gmail.com"},
-    {displayName:"Just a message away!", subtitle:"Instant communication via WhatsApp", link:"https://api.whatsapp.com/send?phone=919769419289&text=Hey%20Scarlet!"}
+  { displayName: 'Home', subtitle: 'Know everything about us.', link: '/' },
+  {
+    displayName: 'About',
+    subtitle: 'Why a conceptualization studio?',
+    link: '/about',
+  },
+  {
+    displayName: 'Services',
+    subtitle: 'What all can we do together?',
+    link: '/services',
+  },
+  {
+    displayName: 'Clients',
+    subtitle: 'You are in a good company.',
+    link: '/clients',
+  },
+  {
+    displayName: 'Mail Us',
+    subtitle: 'hello@bescarlet.com',
+    link: 'mailto:hello@bescarlet.com',
+  },
+  {
+    displayName: 'Just a message away!',
+    subtitle: 'Click to chat on WhatsApp',
+    link:
+      'https://api.whatsapp.com/send?phone=919769419289&text=Hey%20Scarlet!',
+  },
 ];
 
+function BottomPeak(props) {
+  const [showMenu, changeShowMenu] = useState(false);
 
-function BottomPeak(props){
-
-    const [showMenu, changeShowMenu] = useState(false);
-
-    return(
-        <div
-            onMouseLeave={()=> {changeShowMenu(false)}}
-            onMouseEnter={()=> {changeShowMenu(true)}}
-            className={`bottom-peak-container-${props.theme}`}>
-            {
-                showMenu?
-                    <React.Fragment>
-                        <div className={'menu'}>
-                        {
-                            Menu.map(menu => (
-                                <a href={menu.link}>
-                                    <h1>  {menu.displayName}
-                                        <h3> {menu.subtitle} </h3>
-                                    </h1>
-                                </a>
-                            ))
-                        }
-                    </div>
-                    <div className={'quote'}>
-                        <p> It is better to travel well than to arrive. </p>
-                        <author> Buddha </author>
-                    </div>
-                    </React.Fragment>
-                    :null
-            }
-        </div>
-    );
+  return (
+    <div
+      onMouseLeave={() => {
+        changeShowMenu(false);
+      }}
+      onMouseEnter={() => {
+        changeShowMenu(true);
+      }}
+      className={`bottom-peak-container-${props.theme}`}
+    >
+      {showMenu ? (
+        <React.Fragment>
+          <div className={'menu'}>
+            {Menu.map((menu) => (
+              <a href={menu.link}>
+                <h1>
+                  {' '}
+                  {menu.displayName}
+                  <h3> {menu.subtitle} </h3>
+                </h1>
+              </a>
+            ))}
+          </div>
+          <div className={'quote'}>
+            <p> It is better to travel well than to arrive. </p>
+            <author> Buddha </author>
+          </div>
+        </React.Fragment>
+      ) : null}
+    </div>
+  );
 }
 
 export default BottomPeak;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,31 +1,39 @@
-import React, {useState} from 'react';
-import BottomPeak from "../../components/BottomPeak";
-import Frame from "../Frame/Frame";
-import StudioConcept from "../StudioConcept/StudioConcept";
-import Back from "../../components/Back";
-
+import React, { useState } from 'react';
+import BottomPeak from '../../components/BottomPeak';
+import Frame from '../Frame/Frame';
+import StudioConcept from '../StudioConcept/StudioConcept';
+import Back from '../../components/Back';
 
 function Home() {
+  const [currentState, changeState] = useState('');
 
-    const [currentState, changeState] = useState('');
-
-    return(
-        <div className={'home-container container-fluid'}>
-            <StudioConcept currentState={currentState}/>
-            <BottomPeak changeState={changeState} theme={'primary'}/>
-            <Frame theme={'primary'}/>
-            <Back theme={'primary'} changeState={changeState}/>
-            <div className={'row inner-container'}>
-                <div className={'col-sm-2'}/>
-                <div className={'col-sm-7'}>
-                    <h1 className={'title'}> Conceptualisation & Implementation <span onClick={()=>changeState('-active')} className={'alt'}> Studio </span> </h1>
-                    <h3> A conceptualisation studio that expertises in advertising, branding
-                        and product development. </h3>
-                </div>
-                <div className={'col-sm-3'}/>
-            </div>
+  return (
+    <div className={'home-container container-fluid'}>
+      <StudioConcept currentState={currentState} />
+      <BottomPeak changeState={changeState} theme={'primary'} />
+      <Frame theme={'primary'} />
+      <Back theme={'primary'} changeState={changeState} />
+      <div className={'row inner-container'}>
+        <div className={'col-sm-2'} />
+        <div className={'col-sm-7'}>
+          <h1 className={'title'}>
+            {' '}
+            Conceptualisation & Implementation{' '}
+            <span onClick={() => changeState('-active')} className={'alt'}>
+              {' '}
+              Studio{' '}
+            </span>{' '}
+          </h1>
+          <h3>
+            {' '}
+            A conceptualisation studio that expertises in advertising, branding
+            and product development.{' '}
+          </h3>
         </div>
-    );
+        <div className={'col-sm-3'} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
